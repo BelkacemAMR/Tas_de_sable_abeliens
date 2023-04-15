34d049b9df06d26a327754dac3838ec806cd696f
Belkacem <abel-and-co@hotmail.com>
2023-04-15 23:05:39 +0200

    Version Finale

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def apply_gravity(sandpile):
+    while True:
+        unstable_piles = np.where(sandpile >= 4)
+        if len(unstable_piles[0]) == 0:
+           break
+        for i, j in zip(unstable_piles[0], unstable_piles[1]):
+            sandpile[i, j] -= 4
+            for ni, nj in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                if ni >= 0 and ni < sandpile.shape[0] and nj >= 0 and nj < sandpile.shape[1]:
+                    sandpile[ni, nj] += 1
