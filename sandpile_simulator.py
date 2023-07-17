6700c3101c30ec483d135e57c55b884746c4030e
Belkacem <abel-and-co@hotmail.com>
2023-07-17 19:19:15 +0200

    Version finale + test

--- sandpile_simulator.py
+++ sandpile_simulator.py
@@ -9,3 +9,18 @@ def apply_gravity(sandpile):
             for ni, nj in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
                 if ni >= 0 and ni < sandpile.shape[0] and nj >= 0 and nj < sandpile.shape[1]:
                     sandpile[ni, nj] += 1
+
+
+
+
+
+# Créer une pile de sable 3x3 pour tester
+sandpile = np.array([[0, 0, 0],
+                     [0, 4, 0],
+                     [0, 0, 0]])
+
+# Appeler la fonction apply_gravity pour tester
+apply_gravity(sandpile)
+
+# Afficher le résultat
+print(sandpile)
